5fe8ab30186367a097e742ccedd0069b681f9bef
jjeongsu <jeongsu.dev@gmail.com>
2023-03-21 22:29:14 +0900

    7.0 Get Selectors : Learn Usage of Selector

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
+import { useRecoilState, useRecoilValue } from "recoil";
 import { createGlobalStyle } from "styled-components";
+import { hourSelector, minuteState } from "./atoms";
 
 
 function App(){
-  return (
-    <>
+  const [minutes, setMinutes] = useRecoilState(minuteState);
+  const hours = useRecoilValue(hourSelector);
+  const onMinutesChange = (event:React.FormEvent<HTMLInputElement>) => {
+    setMinutes(+event.currentTarget.value); //+는 string을 number로 바꿔줌 ex)+"1"은 1
+  };
 
-    </>
+
+  return (
+    <div>
+      <input value={minutes} onChange={onMinutesChange} type="number" placeholder="Minutes" />
+      <input value={hours} type="number" placeholder="Hours" />
+    </div>
   )
 }
 
--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,1 +1,16 @@
 import {atom, selector} from "recoil";
+
+export const minuteState = atom<number>({
+  key: "minute",
+  default: 0,
+})
+
+export const hourSelector = selector({
+  key: "hours",
+  get:({get}) => {
+    const minutes = get(minuteState); //get을 말그대로 아톰을 가지고 온다.
+    return minutes / 60;
+  }
+})
+
+
