460b8ecd9e509fd0ed08de551d860f04b9416e2a
jjeongsu <jeongsu.dev@gmail.com>
2023-03-22 08:15:51 +0900

    7.1 Set Selectors : hour에서 minute의 atom 조작하기

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,22 @@ import { hourSelector, minuteState } from "./atoms";
 
 function App(){
   const [minutes, setMinutes] = useRecoilState(minuteState);
-  const hours = useRecoilValue(hourSelector);
+  const [hours, setHours] = useRecoilState(hourSelector);
+  //useRecoilState(selector)
+  //hours -> selector의 get으로 부터 온다.
+  //setHours -> selector의 set으로 부터 온다. set을 실행시키는 함수이다.
+  
   const onMinutesChange = (event:React.FormEvent<HTMLInputElement>) => {
     setMinutes(+event.currentTarget.value); //+는 string을 number로 바꿔줌 ex)+"1"은 1
   };
-
+  const onHoursChange = (event:React.FormEvent<HTMLInputElement>) => {
+    setHours(+event.currentTarget.value); //시간단위의 값이 newValue에 전달된다.
+  }
 
   return (
     <div>
       <input value={minutes} onChange={onMinutesChange} type="number" placeholder="Minutes" />
-      <input value={hours} type="number" placeholder="Hours" />
+      <input value={hours} onChange={onHoursChange} type="number" placeholder="Hours" />
     </div>
   )
 }
--- src/atoms.tsx
+++ src/atoms.tsx
@@ -5,12 +5,19 @@ export const minuteState = atom<number>({
   default: 0,
 })
 
-export const hourSelector = selector({
+export const hourSelector = selector<number>({ //selector의 리턴타입은 넘버
   key: "hours",
   get:({get}) => {
-    const minutes = get(minuteState); //get을 말그대로 아톰을 가지고 온다.
+    const minutes = get(minuteState); //get을 말그대로 아톰에 접급하여 가지고 온다.
     return minutes / 60;
-  }
+  },
+  //set은 원하는 아톰의 state을 수정한다.
+  //newValue는 우리가 보내주는 새로운 값.
+  set: ({set}, newValue) => {
+    console.log(newValue);
+    const minutes = Number(newValue) * 60;
+    set(minuteState, minutes)// set(수정하고싶은 아톰, 수정할 값)
+  },
 })
 
 
