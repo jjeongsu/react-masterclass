b74125520b36e1a01dbe80667b5429268b0a65a2
jjeongsu <jeongsu.dev@gmail.com>
2023-09-14 11:15:21 +0900

    README todolist 정리

--- README.md
+++ README.md
@@ -1,11 +1,38 @@
 # Nomad Coder
+
 ## React Master Class 강의를 보고 작성된 코드와 기록들 입니다.
 
-## 사용스택 
+## 사용된 기술 스택
+
 - TypeScript
 - React
 - Styled-Component
 
 ## 알아둘것
- - 전체 소스코드를 포함합니다.
- - 중간중간 정리된 내용은 Memo폴더에 기록됩니다.
+
+- 공부내용은 Memo폴더 혹은 velog에 기록됩니다.
+
+## 다음과 같은 것들을 만들고 배웁니다.
+
+- 코인 tracker
+- todolist (with recoil)
+- trello
+
+### 코인 tracker
+
+<img width="200" alt="coin-mainpage" src="https://github.com/jjeongsu/react-masterclass/assets/92345556/6a445d96-f334-470b-91a8-f93080964d54">
+<img width="200" alt="coin-mainpage-light" src="https://github.com/jjeongsu/react-masterclass/assets/92345556/c8bdd5bb-a9e1-4c43-b685-7a98f95790e2">
+<img width="200" alt="coin-details" src="https://github.com/jjeongsu/react-masterclass/assets/92345556/c6bb5578-fc67-4433-9e4c-05fd6c32ddb1">  
+  
+- react-query를 활용한 data patching, caching
+- styled-componet global style, theme 적용
+- react-apexcharts 라이브러리를 활용한 데이터 시각화
+
+### Todo list
+
+<img width="500" alt="doing" src="https://github.com/jjeongsu/react-masterclass/assets/92345556/370a1947-2eac-4eaf-84e3-0af4e3750bbe">
+<img width="500" alt="todo" src="https://github.com/jjeongsu/react-masterclass/assets/92345556/b4f7ec87-6777-4d41-88ee-83795ae7f6ca">
+<img width="500" alt="done" src="https://github.com/jjeongsu/react-masterclass/assets/92345556/fcbe0926-4d1c-4f1c-990d-d89b98224f50">
+
+- reccoil을 이용하여 전역으로 상태관리하기
+  - recoil의 selector 기능 활용하기
--- src/Components/Todo.tsx
+++ src/Components/Todo.tsx
@@ -1,55 +1,90 @@
-import { useSetRecoilState } from "recoil";
-import styled from "styled-components";
-import { Categories, IToDo, toDoState } from "../atoms";
-const List = styled.li`
-  
-`;
+import { useSetRecoilState } from 'recoil'
+import styled from 'styled-components'
+import { Categories, IToDo, toDoState } from '../atoms'
+const List = styled.li``
 
 /**
  * Todos의 값을 변경하기
  * 중요한점은 state를 mutate하지말고 새로운 state를 만들어야 한다는 것.
  *  1. find todo based on Id, index
- *  
+ *
  */
 
-function ToDo({text, category, id}:IToDo){
-  const setToDos = useSetRecoilState(toDoState);
+function ToDo({ text, category, id }: IToDo) {
+  const setToDos = useSetRecoilState(toDoState)
   const onClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     const {
-      currentTarget: {name} //어떤 버튼을 눌렀는데? 
-    } = e;
-    setToDos((oldTodos) => {
-      const targetIndex = oldTodos.findIndex(todo => todo.id === id); //target의 경로 찾기
-      const oldTodo = oldTodos[targetIndex];
-      const newTodo = {text, id, category: name as any};
+      currentTarget: { name }, //어떤 버튼을 눌렀는데?
+    } = e
+    setToDos(oldTodos => {
+      const targetIndex = oldTodos.findIndex(todo => todo.id === id) //target의 경로 찾기
+      const oldTodo = oldTodos[targetIndex]
+      const newTodo = { text, id, category: name as any }
       //배열의 원소를 교체하기 : 새로운 배열을 만들어서 리턴하기
       //final = [...front, newelement, ...back]
-      return  [
-        ...oldTodos.slice(0,targetIndex),
+      return [
+        ...oldTodos.slice(0, targetIndex),
         newTodo,
-        ...oldTodos.slice(targetIndex+1),
-      ];
-    });
+        ...oldTodos.slice(targetIndex + 1),
+      ]
+    })
   }
   const onDeleteClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     const {
-      currentTarget: {name}
-    } = e;
-    setToDos((oldTodos) => {
-      const targetIndex = oldTodos.findIndex(todo => todo.id === id);
+      currentTarget: { name },
+    } = e
+    setToDos(oldTodos => {
+      const targetIndex = oldTodos.findIndex(todo => todo.id === id)
       return [
         ...oldTodos.slice(0, targetIndex),
-        ...oldTodos.slice(targetIndex + 1)
-      ];
-    });
+        ...oldTodos.slice(targetIndex + 1),
+      ]
+    })
   }
   return (
-  <li >{text } 
-    {category !== Categories.TO_DO && <button name={Categories.TO_DO} onClick={onClick}>DOING </button>}
-    {category !== Categories.DOING &&  <button name={Categories.DOING} onClick={onClick}>DONE </button>}
-    {category !== Categories.DONE && <button name={Categories.DONE} onClick={onClick}>TO_DO </button>}
-    <button name="DELETE" onClick={onDeleteClick}> 삭제하기 </button>
-  </li>);
+    <TodoElement>
+      <span>{text}</span>
+      {category !== Categories.TO_DO && (
+        <Button name={Categories.TO_DO} onClick={onClick}>
+          DOING{' '}
+        </Button>
+      )}
+      {category !== Categories.DOING && (
+        <Button name={Categories.DOING} onClick={onClick}>
+          DONE{' '}
+        </Button>
+      )}
+      {category !== Categories.DONE && (
+        <Button name={Categories.DONE} onClick={onClick}>
+          TO_DO{' '}
+        </Button>
+      )}
+      <Button name="DELETE" onClick={onDeleteClick}>
+        {' '}
+        삭제하기{' '}
+      </Button>
+    </TodoElement>
+  )
 }
 
-export default ToDo;
+export default ToDo
+
+const Button = styled.button`
+  width: 5rem;
+  height: 1.5rem;
+  border: none;
+  background-color: transparent;
+  border: 1px solid purple;
+  border-radius: 0.75rem;
+  margin: 5px;
+  &:hover {
+    color: purple;
+  }
+`
+
+const TodoElement = styled.li`
+  width: 30em;
+  span {
+    margin-right: 10px;
+  }
+`
